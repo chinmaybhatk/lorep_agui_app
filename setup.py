5801e41cb0ae67f5236a924edc801757045af576
Chinmay Bhatk <chinmaybhatk@gmail.com>
2025-09-27 17:18:48 +0530

    Fix pip dependency error
    - Remove frappe from requirements.txt (already installed in environment)
    - Frappe apps should not include frappe as dependency
    - This fixes: Could not find a version that satisfies the requirement frappe

--- setup.py
+++ setup.py
@@ -15,5 +15,6 @@ setup(
 	packages=find_packages(),
 	zip_safe=False,
 	include_package_data=True,
-	install_requires=install_requires
+	install_requires=install_requires,
+	# Frappe is already installed in the environment, don't include it as dependency
 )
